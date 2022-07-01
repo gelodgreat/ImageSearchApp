e76f913a4eb4d890df9eec912a535d9ad7497e32
Angelo Umali <20021423+gelodgreat@users.noreply.github.com>
2022-07-02 01:30:41 +0800

    Implemented redux thunk properly

--- src/redux/reducer/images.ts
+++ src/redux/reducer/images.ts
@@ -1,10 +1,23 @@
 export const imageListReducer = (
   state = {
-    images: {},
+    images: [],
+    isLoading: false,
+    error: null,
   },
   action: any,
 ) => {
   switch (action.type) {
+    case 'LOAD_IMAGES_START':
+      return {...state, isLoading: true};
+    case 'LOAD_IMAGES_SUCCESS':
+      const localImages = state?.images || [];
+      return {
+        ...state,
+        images: [...action.payload, ...localImages],
+        isLoading: false,
+      };
+    case 'LOAD_IMAGES_FAILURE':
+      return {...state, error: action.payload, isLoading: false};
     case 'UPDATE_IMAGES_RESULT': {
       return {...state, images: action.images};
     }
